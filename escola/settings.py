249b6e87f2853268b4dcecae03ae97b634847f9b
fabio1008 <fabiocarini2003@gmail.com>
2022-03-31 23:35:53 -0300

    Pytest-Decouple

--- escola/settings.py
+++ escola/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yd&*dg#$uolgu@_$pkyhu!+%!-v7wdq2q!m$rll_(w2kso+^+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
